10b79fe0de6093a1ce821d3c4f120668561c1f8a
Arnaud ROMIL <arnaud.romil@gmail.com>
2025-05-02 15:12:52 +0200

    add backend websocket config

---
+++ back/src/main/java/com/ycyw/chat_poc/controllers/MessageController.java
@@ -0,0 +1,17 @@
+package com.ycyw.chat_poc.controllers;
+
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MessageController {
+
+    @MessageMapping("/chat.send.{chatId}")
+    @SendTo("/topic/chat.{chatId}")
+    public String sendMessage(@DestinationVariable String chatId, String message) {
+        System.out.println(message + " => " + chatId);
+        return message;
+    }
+}
